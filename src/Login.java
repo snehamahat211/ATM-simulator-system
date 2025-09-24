55ff52822e0f0777c6e671d048eca7653b297bef
snehamahat211 <snehamahat211@gmail.com>
2025-09-24 21:04:10 +0545

    cardno and pincreated

--- src/Login.java
+++ src/Login.java
@@ -24,10 +24,18 @@ public class Login extends JFrame {
         cardno.setBounds(120,150,400,40);
         add(cardno);
 
+        JTextField cardTextField=new JTextField();
+        cardTextField.setBounds(300,150,250,30);
+        add (cardTextField);
+
         JLabel pin =new JLabel("PIN:");
         pin.setFont(new Font("Arial",Font.BOLD,38));
         pin.setBounds(120,220,400,40);
         add(pin);
+
+        JTextField pinTextField=new JTextField();
+        pinTextField.setBounds(300,220,250,30);
+        add (pinTextField);
 
 
         getContentPane().setBackground(Color.white);
