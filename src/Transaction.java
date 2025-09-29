a085a1b5a898e7c97f9bab1180f9e2367421998f
snehamahat211 <snehamahat211@gmail.com>
2025-09-29 22:03:10 +0545

    atm frame image added

---
+++ src/Transaction.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Transaction extends JFrame {
+    Transaction() {
+        setLayout(null);
+        ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("atm.jpg"));
+        Image i2=i1.getImage().getScaledInstance(900,900,Image.SCALE_DEFAULT);
+        ImageIcon i3=new ImageIcon(i2);
+        JLabel image=new JLabel(i3);
+        image.setBounds(0,0,900,900);
+        add(image);
+
+        add(image);
+        setSize(900,900);
+        setLocation(300,0);
+        setVisible(true);
+
+
+    }
+    public static void main(String args[]){
+        new Transaction();
+
+    }
+
+
+}
